bd4a0a10679ac9d481eb48fb797b6853f4bad594
Kieren Davies <kieren@kdavi.es>
2014-09-15 12:56:54 +0300

    fix scripts to be run from anywhere

--- getversion.sh
+++ getversion.sh
@@ -3,7 +3,7 @@
 if [ -f $(dirname $0)/version ]; then
     version=`cat $(dirname $0)/version`
 else
-    version=`mvn help:evaluate -Dexpression=project.version | grep "^[0-9]\{1,\}\."`
+    version=`mvn -f $(dirname $0)/pom.xml help:evaluate -Dexpression=project.version | grep "^[0-9]\{1,\}\."`
     echo $version > $(dirname $0)/version
 fi
 
