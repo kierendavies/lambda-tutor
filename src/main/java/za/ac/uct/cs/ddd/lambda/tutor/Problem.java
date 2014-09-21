fd18d6ee176e4b99a8b7258ab9d35fbaf8445002
David Dunn <david.dunn1603@gmail.com>
2014-09-21 18:45:09 +0200

    Expanded the Problem constructors.

--- src/main/java/za/ac/uct/cs/ddd/lambda/tutor/Problem.java
+++ src/main/java/za/ac/uct/cs/ddd/lambda/tutor/Problem.java
@@ -2,35 +2,88 @@ package za.ac.uct.cs.ddd.lambda.tutor;
 
 import za.ac.uct.cs.ddd.lambda.evaluator.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A wrapper around LambdaExpression for tracking the progress of reductions.
  */
 public abstract class Problem {
 
-    LambdaExpression expression;
-    String message;
-    double mark;
+    protected LambdaExpression expression;
+    protected ReductionOrder reductionOrder;
+    protected List<ReductionResult> reductions;
+    protected List<String> messages;
+    protected int mark;
+    protected int totalMark;
 
     /**
      * Creates a problem from a string by parsing the string into a LambdaExpression.
      * @param startExpression A string representing the starting expression of the problem.
+     * @param order The order in which the expressions in this problem must be reduced.
      */
-    public Problem(String startExpression){
-        mark = 0;
+    public Problem(String startExpression, ReductionOrder order){
         try {
             expression = Parser.parse(startExpression);
         } catch (InvalidExpressionException e) {
             e.printStackTrace();
         }
+        reductionOrder = order;
+        reductions = expression.reductions(order);
+        messages = new ArrayList<>();
+        mark = 0;
+        totalMark = 0;
     }
 
     /**
      * Creates a problem from a LambdaExpression reference.
      * @param startExpression A LambdaExpression representing the starting expression of the problem.
+     * @param order The order in which the expressions in this problem must be reduced.
      */
-    public Problem(LambdaExpression startExpression){
+    public Problem(LambdaExpression startExpression, ReductionOrder order){
+        expression = startExpression.clone();
+        reductionOrder = order;
+        reductions = expression.reductions(order);
+        messages = new ArrayList<>();
+        mark = 0;
+        totalMark = 0;
+    }
+
+    /**
+     * Creates a problem from a string by parsing the string into a LambdaExpression.
+     * @param startExpression A string representing the starting expression of the problem.
+     * @param order The order in which the expressions in this problem must be reduced.
+     * @param maxIterations The maximum number of iterations that should be done when reducing the expression.
+     */
+    public Problem(String startExpression, ReductionOrder order, int maxIterations){
+        try {
+            expression = Parser.parse(startExpression);
+        } catch (InvalidExpressionException e) {
+            e.printStackTrace();
+        }
+        reductionOrder = order;
+        reductions = expression.reductions(order, maxIterations);
+        messages = new ArrayList<>();
+        mark = 0;
+        totalMark = 0;
+    }
+
+    /**
+     * Creates a problem from a LambdaExpression reference.
+     * @param startExpression A LambdaExpression representing the starting expression of the problem.
+     * @param order The order in which the expressions in this problem must be reduced.
+     * @param maxIterations The maximum number of iterations that should be done when reducing the expression.
+     */
+    public Problem(LambdaExpression startExpression, ReductionOrder order, int maxIterations){
         mark = 0;
         expression = startExpression.clone();
+        reductionOrder = order;
+        reductions = expression.reductions(order, maxIterations);
+        messages = new ArrayList<>();
+        mark = 0;
+        totalMark = 0;
     }
 
     /**
