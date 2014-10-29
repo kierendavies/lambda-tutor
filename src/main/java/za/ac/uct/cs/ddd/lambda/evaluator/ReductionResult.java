08c6903117ae902daf4abbda1176e3f1063b33f7
Kieren Davies <kieren@kdavi.es>
2014-10-29 10:03:45 +0200

    license under GPL v3

--- src/main/java/za/ac/uct/cs/ddd/lambda/evaluator/ReductionResult.java
+++ src/main/java/za/ac/uct/cs/ddd/lambda/evaluator/ReductionResult.java
@@ -1,3 +1,21 @@
+/*
+ * Lambda Tutor
+ * Copyright (C) 2014  Kieren Davies, David Dunn, Matthew Dunk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package za.ac.uct.cs.ddd.lambda.evaluator;
 
 import static za.ac.uct.cs.ddd.lambda.evaluator.ReductionType.ALPHA;
